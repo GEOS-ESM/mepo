5b9bc3180a1bce3f672e3e326ba32bc49c7dc6c0
Purnendu Chakraborty <purnendu.chakraborty@nasa.gov>
2019-10-29 19:33:42 -0400

    Bug fix

--- mepo.d/branch/create/create.py
+++ mepo.d/branch/create/create.py
@@ -4,15 +4,18 @@ import subprocess as sp
 
 from mepo_state import MepoState
 
-def run(branch_name):
+def run(repo_names, branch_name):
     allrepos = MepoState.read_state()
-    for repo in allrepos:
-        __create_branch(repo, branch_name)
+    for reponame in repo_names:
+        if reponame not in allrepos:
+            sys.exit('ERROR: invalid repo name [%s]' % reponame)
+        create_branch(reponame, allrepos[reponame], branch_name)
 
-def __create_branch(repo, branch_name):
+def create_branch(reponame, repo, branch_name):
     cwd = os.getcwd()
-    os.chdir(repo['path'])
-    print repo['name'],
+    os.chdir(repo['local'])
+    print reponame+':',
+    sys.stdout.flush()
     cmd = 'git checkout -b %s' % branch_name
     sp.check_output(cmd.split())
     os.chdir(cwd)
