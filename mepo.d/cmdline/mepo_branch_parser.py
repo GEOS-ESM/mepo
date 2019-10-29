ea7c552a0f841ac206ce4a2f6e79504ca8561e02
Purnendu Chakraborty <purnendu.chakraborty@nasa.gov>
2019-10-29 18:15:10 -0400

    Deactivated all branch commands except create

--- mepo.d/cmdline/mepo_branch_parser.py
+++ mepo.d/cmdline/mepo_branch_parser.py
@@ -6,44 +6,44 @@ class MepoBranchParser(object):
     def __init__(self, branch):
         self.branch_subparsers = branch.add_subparsers(dest='mepo_branch_cmd')
         self.__create()
-        self.__push()
-        self.__pull()
-        self.__switch()
-        self.__sync()
+        # self.__push()
+        # self.__pull()
+        # self.__switch()
+        # self.__sync()
         
     def __create(self):
         branch_create = self.branch_subparsers.add_parser(
             'create',
-            description = 'Create branch <branch-name> in specified repos')
+            description = 'Create branch <branch-name> in repo <repo-name>')
         branch_create.add_argument('branch_name', metavar='branch-name')
         branch_create.add_argument(
-            'repos',
-            metavar='in-repos',
+            'repo_name',
+            metavar = 'repo-name', 
             nargs = '+',
             help = 'Repo(s) to create the branch in')
 
-    def __push(self):
-        branch_push = self.branch_subparsers.add_parser(
-            'push',
-            description='Push branch <branch-name> to origin')
-        branch_push.add_argument('branch_name', metavar='branch-name')
+    # def __switch(self):
+    #     branch_switch = self.branch_subparsers.add_parser(
+    #         'switch',
+    #         description='switch repository <repo-name> to branch <branch-name>')
+    #     branch_switch.add_argument('repo_name', metavar='repo-name')
+    #     branch_switch.add_argument('branch_name', metavar='branch-name')
 
-    def __pull(self):
-        branch_pull = self.branch_subparsers.add_parser(
-            'pull',
-            description='Pull branch <branch-name> from origin to sandbox')
-        branch_pull.add_argument('branch_name', metavar='branch-name')
+    # def __push(self):
+    #     branch_push = self.branch_subparsers.add_parser(
+    #         'push',
+    #         description='Push branch <branch-name> to origin')
+    #     branch_push.add_argument('branch_name', metavar='branch-name')
 
-    def __switch(self):
-        branch_switch = self.branch_subparsers.add_parser(
-            'switch',
-            description='switch repository <repo-name> to branch <branch-name>')
-        branch_switch.add_argument('repo_name', metavar='repo-name')
-        branch_switch.add_argument('branch_name', metavar='branch-name')
+    # def __pull(self):
+    #     branch_pull = self.branch_subparsers.add_parser(
+    #         'pull',
+    #         description='Pull branch <branch-name> from origin to sandbox')
+    #     branch_pull.add_argument('branch_name', metavar='branch-name')
 
-    def __sync(self):
-        branch_sync = self.branch_subparsers.add_parser(
-            'sync',
-            description = 'sync branch <branch-name> with remote')
-        branch_sync.add_argument('branch_name', metavar='branch-name')
+    # def __sync(self):
+    #     branch_sync = self.branch_subparsers.add_parser(
+    #         'sync',
+    #         description = 'sync branch <branch-name> with remote')
+    #     branch_sync.add_argument('branch_name', metavar='branch-name')
         
