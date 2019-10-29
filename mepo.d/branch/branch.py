c11eb66f83aeefa70414cc9838b72f4fce23d1c7
Purnendu Chakraborty <purnendu.chakraborty@nasa.gov>
2019-10-29 18:20:32 -0400

    mepo branch create is now active

--- mepo.d/branch/branch.py
+++ mepo.d/branch/branch.py
@@ -5,10 +5,8 @@ from switch import switch
 def run(args):
     branch_cmd = args.mepo_branch_cmd
     branch_name = args.branch_name
-    raise NotImplementedError('"mepo branch %s" has not yet been implemented' % branch_cmd)
-    # if branch_cmd == 'create':
-    #     create.run(branch_name)
-    # elif branch_cmd == 'switch':
-    #     switch.run(args.repo_name, args.branch_name)
-    # else:
-    #     raise NotImplementedError('"mepo branch %s" has not yet been implemented' % branch_cmd)
+    repo_names = args.repo_name
+    if branch_cmd == 'create':
+        create.run(repo_names, branch_name)
+    else:
+        raise NotImplementedError('"mepo branch %s" has not yet been implemented' % branch_cmd)
