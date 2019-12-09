26ab2fb2911696f8102e0dfe4a32bc69df847c90
Purnendu Chakraborty <purnendu.chakraborty@nasa.gov>
2019-12-09 17:54:05 -0500

    Cleaned code to checkout branches

--- mepo.d/command/checkout/checkout.py
+++ mepo.d/command/checkout/checkout.py
@@ -1,25 +1,23 @@
-import os
-
 from state.state import MepoState
 from utilities import shellcmd
+from utilities import verify
 
 def run(args):
-    repo_name = args.repo_name
-    create = args.b
-    branch_name = args.branch_name
     allrepos = MepoState.read_state()
-    for reponame in repo_name:
-        if reponame not in allrepos:
-            raise Exception('invalid repo name [%s]' % reponame)
-        if create:
-            _create_branch(reponame, allrepos[reponame], branch_name)
-        _checkout_branch(reponame, allrepos[reponame], branch_name)
+    verify.valid_repos(args.repo_name, allrepos.keys())
+    repos_co = {name: allrepos[name] for name in args.repo_name}
+    for name, repo in repos_co.items():
+        local_path = repo['local']
+        branch_name = args.branch_name
+        if args.b:
+            _create_branch(local_path, branch_name)
+            print('+ {}: {}'.format(name, branch_name))
+        _checkout_branch(local_path, branch_name)
 
-def _checkout_branch(name, repo, branch):
-    cmd = 'git -C %s checkout %s' % (repo['local'], branch)
-    shellcmd.run(cmd.split(), output=True).strip()
-
-def _create_branch(reponame, repo, branch):
-    cmd = 'git -C %s branch %s' % (repo['local'], branch)
+def _checkout_branch(local_path, branch):
+    cmd = 'git -C {} checkout {}'.format(local_path, branch)
     shellcmd.run(cmd.split())
-    print('+ {}: {}'.format(reponame, branch))
+
+def _create_branch(local_path, branch):
+    cmd = 'git -C {} branch {}'.format(local_path, branch)
+    shellcmd.run(cmd.split())
