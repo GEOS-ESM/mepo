7a2d0118b1cf21cbc8ff2e3ee333b21eac881a7f
Purnendu Chakraborty <purnendu.chakraborty@nasa.gov>
2019-12-20 16:53:41 -0500

    Improved performance of 'mepo status'

--- mepo.d/command/status/status.py
+++ mepo.d/command/status/status.py
@@ -1,19 +1,30 @@
+import sys
+import time
+import multiprocessing as mp
+
 from state.state import MepoState
 from repository.git import GitRepository
 from utilities.version import version_to_string
 
 def run(args):
+    print('Checking status...'); sys.stdout.flush()
     allcomps = MepoState.read_state()
-    max_namelen = len(max([comp.name for comp in allcomps], key=len))
-    for comp in allcomps:
-        git = GitRepository(comp.remote, comp.local)
-        curr_ver = version_to_string(git.get_version())
-        output = git.check_status()
-        print_status(comp.name, curr_ver, output, max_namelen)
+    pool = mp.Pool()
+    result = pool.map(check_component_status, allcomps)
+    print_status(allcomps, result)
 
-def print_status(name, version, output, width):
+def check_component_status(comp):
+    git = GitRepository(comp.remote, comp.local)
+    curr_ver = version_to_string(git.get_version())
+    return (curr_ver, git.check_status())
+
+def print_status(allcomps, result):
+    width = len(max([comp.name for comp in allcomps], key=len))
     FMT0 = '{:<%s.%ss} | {:<s}' % (width, width)
-    print(FMT0.format(name, version))
-    if (output):
-        for line in output.split('\n'):
-            print('   |', line.rstrip())
+    for index, comp in enumerate(allcomps):
+        time.sleep(0.025)
+        current_version, output = result[index]
+        print(FMT0.format(comp.name, current_version))
+        if (output):
+            for line in output.split('\n'):
+                print('   |', line.rstrip())
