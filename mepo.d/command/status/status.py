0fc1c47b60cfb516d06a66421787d6a34f407427
Matthew Thompson <matthew.thompson@nasa.gov>
2020-02-28 13:43:06 -0500

    Colorize component if branch changed from original

--- mepo.d/command/status/status.py
+++ mepo.d/command/status/status.py
@@ -5,6 +5,7 @@ import multiprocessing as mp
 from state.state import MepoState
 from repository.git import GitRepository
 from utilities.version import version_to_string
+from utilities import colors
 
 def run(args):
     print('Checking status...'); sys.stdout.flush()
@@ -19,12 +20,19 @@ def check_component_status(comp):
     return (curr_ver, git.check_status())
 
 def print_status(allcomps, result):
-    width = len(max([comp.name for comp in allcomps], key=len))
-    FMT0 = '{:<%s.%ss} | {:<s}' % (width, width)
+    orig_width = len(max([comp.name for comp in allcomps], key=len))
     for index, comp in enumerate(allcomps):
         time.sleep(0.025)
         current_version, output = result[index]
-        print(FMT0.format(comp.name, current_version))
+        # Check to see if the current tag/branch is the same as the original
+        if comp.version.name not in current_version:
+            component_name = colors.RED + comp.name + colors.RESET
+            width = orig_width + len(colors.RED) + len(colors.RESET)
+        else:
+            component_name = comp.name
+            width = orig_width
+        FMT0 = '{:<%s.%ss} | {:<s}' % (width, width)
+        print(FMT0.format(component_name, current_version))
         if (output):
             for line in output.split('\n'):
                 print('   |', line.rstrip())
