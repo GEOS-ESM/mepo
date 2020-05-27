bda0a93e65bebf44bffaa0349be365acc1542a1d
Matthew Thompson <matthew.thompson@nasa.gov>
2020-05-27 11:30:59 -0400

    Fixes #57. mepo-cd alone moves to root
    This PR should allow `mepo-cd` alone to `cd` to the root of the project
    where 'root' is where `root/.mepo/` exists.

--- mepo.d/command/whereis/whereis.py
+++ mepo.d/command/whereis/whereis.py
@@ -6,10 +6,13 @@ from utilities import verify
 def run(args):
     allcomps = MepoState.read_state()
     if args.comp_name: # single comp name is specified, print relpath
-        verify.valid_components([args.comp_name], allcomps)
-        for comp in allcomps:
-            if comp.name == args.comp_name:
-                print(_get_relative_path(comp.local))
+        if args.comp_name == "_root":
+            print(MepoState.get_root_dir())
+        else:
+            verify.valid_components([args.comp_name], allcomps)
+            for comp in allcomps:
+                if comp.name == args.comp_name:
+                    print(_get_relative_path(comp.local))
     else: # print relpaths of all comps
         max_namelen = len(max([x.name for x in allcomps], key=len))
         FMT = '{:<%s.%ss} | {:<s}' % (max_namelen, max_namelen)
