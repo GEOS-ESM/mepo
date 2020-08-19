8689d95766a8c8b9cdeefd46c997e0f5a4e3aec5
Matthew Thompson <matthew.thompson@nasa.gov>
2020-08-19 14:54:35 -0400

    Add ability to specify a specific branch for listing

--- mepo.d/command/branch/brlist/brlist.py
+++ mepo.d/command/branch/brlist/brlist.py
@@ -1,13 +1,22 @@
 from state.state import MepoState
+from utilities import verify
 from repository.git import GitRepository
 
 def run(args):
     allcomps = MepoState.read_state()
-    max_namelen = len(max([x.name for x in allcomps], key=len))
+    comps2list = _get_comps_to_list(args.comp_name, allcomps)
+    max_namelen = len(max([x.name for x in comps2list], key=len))
     FMT = '{:<%s.%ss} | {:<s}' % (max_namelen, max_namelen)
-    for comp in allcomps:
+    for comp in comps2list:
         git = GitRepository(comp.remote, comp.local)
         output = git.list_branch(args.all).rstrip().split('\n')
         print(FMT.format(comp.name, output[0]))
         for line in output[1:]:
             print(FMT.format('', line))
+
+def _get_comps_to_list(specified_comps, allcomps):
+    comps_to_list = allcomps
+    if specified_comps:
+        verify.valid_components(specified_comps, allcomps)
+        comps_to_list = [x for x in allcomps if x.name in specified_comps]
+    return comps_to_list
