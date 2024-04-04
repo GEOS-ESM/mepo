af8ea631eef6f9ce0d01df41e47c38349f3be9d2
Purnendu Chakraborty <purnendu.chakraborty@nasa.gov>
2024-04-04 11:24:02 -0400

    Renamed main.py as __main__.py, and related changes

--- src/mepo/__main__.py
+++ src/mepo/__main__.py
@@ -1,6 +1,11 @@
 from .cmdline.parser import MepoArgParser
 from .command import command
 
+
 def main():
     args = MepoArgParser().parse()
     command.run(args)
+
+
+if __name__ == "__main__":
+    main()
