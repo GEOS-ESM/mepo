c16bbecba3870f057c1e1e005c3f6d7a5105f687
Purnendu Chakraborty <purnendu.chakraborty@nasa.gov>
2024-04-05 12:57:09 -0400

    Fixed tests

--- tests/test_mepo_commands.py
+++ tests/test_mepo_commands.py
@@ -1,7 +1,7 @@
 import os
 import sys
 THIS_DIR = os.path.dirname(os.path.realpath(__file__))
-sys.path.insert(0, os.path.join(THIS_DIR, '..', 'src'))
+sys.path.insert(0, os.path.join(THIS_DIR, "..", "src"))
 import shutil
 import shlex
 import unittest
@@ -22,38 +22,42 @@ class TestMepoCommands(unittest.TestCase):
 
     @classmethod
     def __checkout_fixture(cls):
-        remote = 'https://github.com/GEOS-ESM/{}.git'.format(cls.fixture)
-        cmd = 'git clone -b {} {} {}'.format(cls.tag, remote, cls.fixture_dir)
+        remote = "https://github.com/pchakraborty/{}.git".format(cls.fixture)
+        git_clone = "git clone "
+        if cls.tag:
+            git_clone += f"-b {cls.tag}"
+        cmd = f"{git_clone} {remote} {cls.fixture_dir}"
+        print(f"clone command: {cmd}")
         sp.run(shlex.split(cmd))
 
     @classmethod
     def __copy_config_file(cls):
-        src = os.path.join(cls.input_dir, 'components.yaml')
+        src = os.path.join(cls.input_dir, "components.yaml")
         dst = os.path.join(cls.fixture_dir)
         shutil.copy(src, dst)
 
     @classmethod
     def setUpClass(cls):
-        cls.input_dir = os.path.join(THIS_DIR, 'input')
-        cls.output_dir = os.path.join(THIS_DIR, 'output')
-        cls.fixture = 'GEOSfvdycore'
-        cls.tag = 'hierarchical-mepo-based-on-v2.13.0'
-        cls.tmpdir = os.path.join(THIS_DIR, 'tmp')
+        cls.input_dir = os.path.join(THIS_DIR, "input")
+        cls.output_dir = os.path.join(THIS_DIR, "output")
+        cls.fixture = "GEOSfvdycore"
+        cls.tag = None
+        cls.tmpdir = os.path.join(THIS_DIR, "tmp")
         cls.fixture_dir = os.path.join(cls.tmpdir, cls.fixture)
         if os.path.isdir(cls.fixture_dir):
             shutil.rmtree(cls.fixture_dir)
         cls.__checkout_fixture()
         #cls.__copy_config_file()
-        args.style = 'prefix'
+        args.style = "prefix"
         os.chdir(cls.fixture_dir)
         args.repo_url = None
         args.branch = None
         args.directory = None
-        args.partial = 'blobless'
-        args.registry = 'components.yaml'
+        args.partial = "blobless"
+        args.registry = "components.yaml"
         mepo_clone(args)
         # In order to better test compare, we need to do *something*
-        args.comp_name = ['env','cmake','fvdycore']
+        args.comp_name = ["env","FMS","fvdycore"]
         args.quiet = False
         mepo_develop(args)
 
@@ -64,7 +68,7 @@ class TestMepoCommands(unittest.TestCase):
         sys.stdout = output = StringIO()
         mepo_list(args)
         sys.stdout = sys.__stdout__
-        with open(os.path.join(self.__class__.output_dir, 'list_output.txt'), 'r') as fin:
+        with open(os.path.join(self.__class__.output_dir, "list_output.txt"), "r") as fin:
             saved_output = fin.read()
         self.assertEqual(output.getvalue(), saved_output)
 
@@ -75,7 +79,7 @@ class TestMepoCommands(unittest.TestCase):
         args.hashes=False
         mepo_status(args)
         sys.stdout = sys.__stdout__
-        with open(os.path.join(self.__class__.output_dir, 'status_output.txt'), 'r') as fin:
+        with open(os.path.join(self.__class__.output_dir, "status_output.txt"), "r") as fin:
             saved_output = fin.read()
         self.assertEqual(output.getvalue(), saved_output)
 
@@ -86,7 +90,7 @@ class TestMepoCommands(unittest.TestCase):
         args.wrap=True
         mepo_compare(args)
         sys.stdout = sys.__stdout__
-        with open(os.path.join(self.__class__.output_dir, 'compare_brief_output.txt'), 'r') as fin:
+        with open(os.path.join(self.__class__.output_dir, "compare_brief_output.txt"), "r") as fin:
             saved_output = fin.read()
         self.assertEqual(output.getvalue(), saved_output)
 
@@ -97,7 +101,7 @@ class TestMepoCommands(unittest.TestCase):
         args.wrap=True
         mepo_compare(args)
         sys.stdout = sys.__stdout__
-        with open(os.path.join(self.__class__.output_dir, 'compare_full_output.txt'), 'r') as fin:
+        with open(os.path.join(self.__class__.output_dir, "compare_full_output.txt"), "r") as fin:
             saved_output = fin.read()
         self.assertEqual(output.getvalue(), saved_output)
 
@@ -109,5 +113,5 @@ class TestMepoCommands(unittest.TestCase):
         os.chdir(THIS_DIR)
         shutil.rmtree(cls.tmpdir)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
