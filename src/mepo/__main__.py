c90136cffc5cfe5ff98ff545836cfa1b8ca8108b
Purnendu Chakraborty <purnendu.chakraborty@nasa.gov>
2024-04-15 11:22:24 -0500

    Code from src/mepo/command/command.py included in src/mepo/__main__.py

--- src/mepo/__main__.py
+++ src/mepo/__main__.py
@@ -1,10 +1,19 @@
+from importlib import import_module
+
 from .cmdline.parser import MepoArgParser
-from .command import command
+from .utilities import mepoconfig
 
 
 def main():
     args = MepoArgParser().parse()
-    command.run(args)
+    # command.run(args)
+    mepo_cmd = mepoconfig.get_alias_command(args.mepo_cmd)
+
+    # Load the module containing the "run" method of specified command
+    cmd_module = import_module(f"mepo.command.{mepo_cmd}")
+
+    # Execute "run" method of specified command
+    cmd_module.run(args)
 
 
 if __name__ == "__main__":
