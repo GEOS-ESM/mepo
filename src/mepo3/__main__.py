c2c09ad2ca0a75ff5fee12b341c6541071f4b5c1
Purnendu Chakraborty <purnendu.chakraborty@nasa.gov>
2024-04-18 15:29:10 -0500

    __main__.py: added module and function docstrings

--- src/mepo3/__main__.py
+++ src/mepo3/__main__.py
@@ -1,3 +1,10 @@
+"""
+Provides a CLI for mepo. __main__.py is executed when the package mepo is
+invoked directly via
+
+> python -m mepo
+"""
+
 from importlib import import_module
 
 from .cmdline.parser import MepoArgParser
@@ -5,6 +12,7 @@ from .utilities import mepoconfig
 
 
 def main():
+    """Parse command line options and execute the specified mepo command"""
     args = MepoArgParser().parse()
     mepo_cmd = mepoconfig.get_alias_command(args.mepo_cmd)
 
