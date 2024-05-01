d5d57b162c9cf096131f1f133819f6d1b14cd92e
Purnendu Chakraborty <purnendu.chakraborty@nasa.gov>
2024-05-01 16:05:51 -0400

    Formatting changes to update-state.py

--- src/mepo/command/update-state.py
+++ src/mepo/command/update-state.py
@@ -4,14 +4,14 @@ from ..component import MepoComponent
 def run(args):
     allcomps_old = MepoState.read_state()
     MepoState.mepo1_patch_undo()
+    # Convert component to dict and then package it as component again. That is
+    # needed to avoid pickle trying to use the path to the old modules
     allcomps = []
     for comp in allcomps_old:
-        # Convert component to dict and then package
-        # it as component again . That is needed toavoid pickle trying to use the path to the old
-        # component module
         tmp_dict = comp.to_dict(None)
         name = list(tmp_dict)[0]
         comp = tmp_dict[name]
         allcomps.append(MepoComponent().to_component(name, comp, None))
+    # Write new state
     MepoState.write_state(allcomps)
     print('\nConverted mepo1 state to mepo2\n')
