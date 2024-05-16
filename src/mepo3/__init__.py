63d8b0337b0a1cba17a685d65712d8f9ce393f41
Purnendu Chakraborty <purnendu.chakraborty@nasa.gov>
2024-05-16 12:57:20 -0400

    Increasing Pylint score

--- src/mepo3/__init__.py
+++ src/mepo3/__init__.py
@@ -1,4 +1,6 @@
+"""Ensure Python version"""
 import sys
 
-if sys.version_info < (3, 9, 0):
-    sys.exit("ERROR: Python version needs to be >= 3.9.0")
+PY_VERSION = (3, 9, 0)
+if sys.version_info < PY_VERSION:
+    sys.exit(f"ERROR: Python version needs to be >= {PY_VERSION}")
