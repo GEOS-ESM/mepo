ae51946682807c3a919148a59835429d76e540c0
Purnendu Chakraborty <purnendu.chakraborty@nasa.gov>
2024-10-02 12:21:28 -0400

    mepo completion doesn't require the extra mepo-path.py anymore

--- etc/mepo-completion.bash
+++ etc/mepo-completion.bash
@@ -1,13 +1,8 @@
 #!/usr/bin/env bash
 
-# complete -W "init clone status checkout branch diff where whereis history" mepo
-
-# SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-SCRIPT_DIR=$(dirname $(realpath $0))
-
 _get_mepo_commands() {
     local mepo_cmd_list=""
-    local mepo_dir=$(python3 $SCRIPT_DIR/mepo-path.py)
+    local mepo_dir=$(python3 -c "import os, mepo; print(os.path.dirname(mepo.__file__))")
     for pyfile in $(ls ${mepo_dir}/command/*.py*); do
 	command=${pyfile##*/} # remove path
 	command=${command%.*} # remove extension
