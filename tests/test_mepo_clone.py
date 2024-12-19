a0e0207898b50f62ce6ff6f6145045436520256a
Purnendu Chakraborty <purnendu.chakraborty@nasa.gov>
2024-12-19 08:13:23 -0600

    Updated test_mepo_clone.py to use https://github.com/pchakraborty/GEOSfvdycore-mepo-testing.git instead

--- tests/test_mepo_clone.py
+++ tests/test_mepo_clone.py
@@ -13,7 +13,8 @@ import mepo.command.clone as mepo_clone
 import mepo.command.status as mepo_status
 
 
-FIXTURE_URL = "https://github.com/GEOS-ESM/GEOSfvdycore.git"
+FIXTURE_NAME = "GEOSfvdycore-mepo-testing"
+FIXTURE_URL = f"https://github.com/pchakraborty/{FIXTURE_NAME}.git"
 
 
 def get_mepo_status():
@@ -29,9 +30,8 @@ def get_mepo_status():
 
 
 def test_mepo_clone_url():
-    DIRECTORY = "GEOSfvdycore"
-    if os.path.isdir(DIRECTORY):
-        shutil.rmtree(DIRECTORY)
+    if os.path.isdir(FIXTURE_NAME):
+        shutil.rmtree(FIXTURE_NAME)
     args = SimpleNamespace(
         style="prefix",
         registry=None,
@@ -44,20 +44,19 @@ def test_mepo_clone_url():
     mepo_clone.run(args)
     saved_output = """Checking status...
 GEOSfvdycore           | (b) main
-env                    | (t) v4.29.0 (DH)
-cmake                  | (t) v3.51.0 (DH)
-ecbuild                | (t) geos/v1.3.0 (DH)
-GMAO_Shared            | (t) v1.9.8 (DH)
-GEOS_Util              | (t) v2.1.2 (DH)
-MAPL                   | (t) v2.48.0 (DH)
+env                    | (t) v4.29.1 (DH)
+cmake                  | (t) v3.55.0 (DH)
+ecbuild                | (t) geos/v1.4.0 (DH)
+GMAO_Shared            | (t) v1.9.9 (DH)
+GEOS_Util              | (t) v2.1.3 (DH)
 FMS                    | (t) geos/2019.01.02+noaff.10 (DH)
 FVdycoreCubed_GridComp | (t) v2.12.0 (DH)
-fvdycore               | (t) geos/v2.9.0 (DH)
+fvdycore               | (t) geos/v2.9.1 (DH)
 """
-    with contextlib_chdir(DIRECTORY):
+    with contextlib_chdir(FIXTURE_NAME):
         status_output = get_mepo_status()
     assert status_output == saved_output
-    shutil.rmtree(DIRECTORY)
+    shutil.rmtree(FIXTURE_NAME)
 
 
 def test_mepo_clone_url_branch_directory():
@@ -68,23 +67,22 @@ def test_mepo_clone_url_branch_directory():
         style="postfix",
         registry=None,
         url=FIXTURE_URL,
-        branch="release/MAPL-v3",
+        branch="fvdycore-develop",
         directory=DIRECTORY,
         partial="blobless",
         allrepos=False,
     )
     mepo_clone.run(args)
     saved_output = """Checking status...
-GEOSfvdycore           | (b) release/MAPL-v3
-env                    | (t) v4.29.0 (DH)
-cmake                  | (t) v3.51.0 (DH)
-ecbuild                | (t) geos/v1.3.0 (DH)
-GMAO_Shared            | (t) v1.9.8 (DH)
-GEOS_Util              | (b) release/MAPL-v3
-MAPL                   | (b) release/MAPL-v3
+GEOSfvdycore           | (b) fvdycore-develop
+env                    | (t) v4.29.1 (DH)
+cmake                  | (t) v3.55.0 (DH)
+ecbuild                | (t) geos/v1.4.0 (DH)
+GMAO_Shared            | (t) v1.9.9 (DH)
+GEOS_Util              | (t) v2.1.3 (DH)
 FMS                    | (t) geos/2019.01.02+noaff.10 (DH)
-FVdycoreCubed_GridComp | (b) release/MAPL-v3
-fvdycore               | (b) geos/release/MAPL-v3
+FVdycoreCubed_GridComp | (b) develop
+fvdycore               | (b) geos/develop
 """
     with contextlib_chdir(DIRECTORY):
         status_output = get_mepo_status()
