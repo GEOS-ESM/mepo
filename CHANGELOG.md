4e9c8550565a1bc29635946d2614ebb852889414
Matthew Thompson <matthew.thompson@nasa.gov>
2025-04-16 07:50:59 -0400

    Fixes #355. Allow for fixture without develop key

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -13,6 +13,12 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ### Changed
 
+## [2.3.1] - 2025-04-16
+
+### Fixed
+
+- Fixed `clone` to allow for fixtures without a `develop:` key in `components.yaml`
+
 ## [2.3.0] - 2025-01-12
 
 ### Changed
--- src/mepo/registry.py
+++ src/mepo/registry.py
@@ -35,9 +35,9 @@ class Registry(object):
         num_fixtures = 0
         for k, v in d.items():
             if "fixture" in v:
-                # In case of a fixture, develop is the only additional key
+                # In case of a fixture, develop is the only additional allowed key
                 num_fixtures += 1
-                assert list(v.keys()) == ["fixture", "develop"]
+                assert list(v.keys()) == ["fixture"] or list(v.keys()) == ["fixture", "develop"]
             else:
                 # For non-fixture, one and only one of branch/tag/hash allowed
                 xsection = git_tag_types.intersection(set(v.keys()))
